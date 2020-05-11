8a7cdc891f7614da3363a4dce96603fd1ca6b4d2
Maxime Daniel <root@maxux.net>
2020-05-11 16:35:10 +0200

    Drop wireguard external modules completely

--- packages/wireguard.sh
+++ packages/wireguard.sh
@@ -1,13 +1,9 @@
 WIREGUARD_TOOLS_VERSION="1.0.20200102"
 WIREGUARD_TOOLS_CHECKSUM="611eb05e295550f8267092bbf2731fd1"
 WIREGUARD_TOOLS_LINK="https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-${WIREGUARD_TOOLS_VERSION}.tar.xz"
-WIREGUARD_MODULES_VERSION="0.0.20200105"
-WIREGUARD_MODULES_CHECKSUM="c2f2d5803623a71a086eab41c29d9b15"
-WIREGUARD_MODULES_LINK="https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-${WIREGUARD_MODULES_VERSION}.tar.xz"
 
 download_wireguard() {
     download_file $WIREGUARD_TOOLS_LINK $WIREGUARD_TOOLS_CHECKSUM
-    # download_file $WIREGUARD_MODULES_LINK $WIREGUARD_MODULES_CHECKSUM
 }
 
 extract_wireguard() {
@@ -15,17 +11,10 @@ extract_wireguard() {
         echo "[+] extracting: wireguard-tools-${WIREGUARD_TOOLS_VERSION}"
         tar -xf ${DISTFILES}/wireguard-tools-${WIREGUARD_TOOLS_VERSION}.tar.xz -C .
     fi
-
-    # if [ ! -d "wireguard-linux-compat-${WIREGUARD_MODULES_VERSION}" ]; then
-    #    echo "[+] extracting: wireguard-linux-compat-${WIREGUARD_MODULES_VERSION}"
-    #    tar -xf ${DISTFILES}/wireguard-linux-compat-${WIREGUARD_MODULES_VERSION}.tar.xz -C .
-    # fi
 }
 
 prepare_wireguard_modules() {
     echo "[+] preparing wireguard (kernel module upstream)"
-    # link wireguard directory into kernel tree
-    # ./kernel-tree-scripts/jury-rig.sh ${WORKDIR}/linux-${KERNEL_VERSION}
 }
 
 compile_wireguard_tools() {
@@ -39,10 +28,6 @@ install_wireguard_tools() {
 }
 
 build_wireguard() {
-    # pushd "${WORKDIR}/wireguard-linux-compat-${WIREGUARD_MODULES_VERSION}"
-    # prepare_wireguard_modules
-    # popd
-
     pushd "${WORKDIR}/wireguard-tools-${WIREGUARD_TOOLS_VERSION}/src"
     compile_wireguard_tools
     install_wireguard_tools
