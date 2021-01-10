92b92a26d0b7ccd98e6b9fce4f3779fa6ed9c1a4
majjihari <majjihari@gmail.com>
2021-01-10 21:17:06 +0300

    changes

--- autobuild/tf-build-upload.sh
+++ autobuild/tf-build-upload.sh
@@ -11,5 +11,5 @@ echo "[+] branch: ${linkname}"
 
 cp vmlinuz.efi "${kernel}"
 
-curl -x -b "token=${BOOTSTRAP_TOKEN}" -X POST -F "kernel=@${kernel}" "https://bootstrap.ogmesh.com/api/kernel"
-curl -x -b "token=${BOOTSTRAP_TOKEN}" "https://bootstrap.ogmesh.com/api/symlink/${linkname}/${kernel}"
+curl -k -b "token=${BOOTSTRAP_TOKEN}" -X POST -F "kernel=@${kernel}" "https://bootstrap.ogmesh.com/api/kernel"
+curl -k -b "token=${BOOTSTRAP_TOKEN}" "https://bootstrap.ogmesh.com/api/symlink/${linkname}/${kernel}"
